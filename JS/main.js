0be3dee7a7d278bec46ae6f327da7cf7858807e8
Michael <michaeldrpabaya@gmail.com>
2023-02-11 14:07:42 +0800

    navigation bar created

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,24 @@
-const me = document.querySelector('sample')
+// menu button
+const navButton = document.getElementsByClassName('nav__menu_button')[0]
+const navButtonIcon = document.getElementsByClassName('nav__menu_button_icon')[0]
+let sliderIsOpen = false
+
+//slider
+const slider = document.getElementsByClassName('nav__menu_mobile')[0]
+
+// console.log(navButton)
+navButton.addEventListener('click', function() {
+
+    if(sliderIsOpen){
+        navButton.classList.remove('active')
+        navButtonIcon.classList.remove('active')
+        slider.classList.remove('active')
+        slider.classList.add('inactive')
+    }else{
+        navButton.classList.add('active')
+        navButtonIcon.classList.add('active')
+        slider.classList.remove('inactive')
+        slider.classList.add('active')
+    }
+    sliderIsOpen = !sliderIsOpen
+})
--- notes.md
+++ notes.md
@@ -1,3 +1,9 @@
+| < 481px               | Mobile devices          |
+| 481px — 768px         | iPads, Tablets          |
+| 769px — 1024px        | Small screens, laptops  |
+| 1025px — 1200px       | Desktops, large screens |
+| 1201px and greater    | Extra large screens, TV |
+
 Abyss theme
 solarized dark
 Dark + v2 (experimental)
