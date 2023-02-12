466257ff7b352692ccbc76351444fe2bbde8f2ae
Michael <michaeldrpabaya@gmail.com>
2023-02-12 18:12:37 +0800

    page two

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 // menu button
 const navButton = document.getElementsByClassName('nav__menu_button')[0]
 const navButtonIcon = document.getElementsByClassName('nav__menu_button_icon')[0]
+const sidebar = document.getElementById('sidebar')
+const sidebarButton = document.getElementById('nav__button')
 let sliderIsOpen = false
 
 //slider
@@ -9,7 +11,7 @@ const slider = document.getElementsByClassName('nav__menu_mobile')[0]
 // slider mobile menu
 const sliderMobileMenu = document.querySelectorAll('.nave__menu_mobile_menu')
 
-// console.log(navButton)
+//mobile menu button 
 navButton.addEventListener('click', function() {
 
     if(sliderIsOpen){
@@ -25,7 +27,7 @@ navButton.addEventListener('click', function() {
     }
     sliderIsOpen = !sliderIsOpen
 })
-
+// close sidebar on click to the menu
 sliderMobileMenu.forEach(slidermobilemenu => {
     slidermobilemenu.addEventListener('click', () => {
         if(sliderIsOpen){
