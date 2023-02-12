2bc201f3e9d1e71681481e72f4023ae7691f706b
Michael <michaeldrpabaya@gmail.com>
2023-02-12 14:48:17 +0800

    Added Bubbles on HeroSection

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,9 @@ let sliderIsOpen = false
 //slider
 const slider = document.getElementsByClassName('nav__menu_mobile')[0]
 
+// slider mobile menu
+const sliderMobileMenu = document.querySelectorAll('.nave__menu_mobile_menu')
+
 // console.log(navButton)
 navButton.addEventListener('click', function() {
 
@@ -22,3 +25,20 @@ navButton.addEventListener('click', function() {
     }
     sliderIsOpen = !sliderIsOpen
 })
+
+sliderMobileMenu.forEach(slidermobilemenu => {
+    slidermobilemenu.addEventListener('click', () => {
+        if(sliderIsOpen){
+            navButton.classList.remove('active')
+            navButtonIcon.classList.remove('active')
+            slider.classList.remove('active')
+            slider.classList.add('inactive')
+        }else{
+            navButton.classList.add('active')
+            navButtonIcon.classList.add('active')
+            slider.classList.remove('inactive')
+            slider.classList.add('active')
+        }
+        sliderIsOpen = !sliderIsOpen
+    })
+});
