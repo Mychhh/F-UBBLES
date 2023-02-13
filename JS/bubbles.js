e6be18c4c23a79a886ddfbd16b60e0d9ebc86f83
Michael <michaeldrpabaya@gmail.com>
2023-02-13 09:33:57 +0800

    page three

--- JS/bubbles.js
+++ JS/bubbles.js
@@ -1,9 +1,9 @@
 let aosBubbles = Array.from(document.querySelectorAll('.bubble'))
 
 window.addEventListener('scroll', scanBubbles)
+window.addEventListener('scroll', scanCardsBubbble)
 
-function scanBubbles(e){
-    console.count(e)
+function scanBubbles(){
     aosBubbles.forEach(bubbles => {
         if(isVisible(bubbles)){
             bubbles.classList.add('active')
@@ -17,4 +17,16 @@ function isVisible(bubbles){
     const bubblesDiv = bubbles.getBoundingClientRect()
     let distance = -10
     return bubblesDiv.top - window.innerHeight < distance ? true : false
+}
+
+let aosCardsBubbles =  Array.from(document.querySelectorAll('.page_three__cards_bubble'))
+
+function scanCardsBubbble(){
+    aosCardsBubbles.forEach(aoscardsbubbles => {
+        if(isVisible(aoscardsbubbles)){
+            aoscardsbubbles.classList.add('active')
+        }else{
+            aoscardsbubbles.classList.remove('active')
+        }
+    })
 }
